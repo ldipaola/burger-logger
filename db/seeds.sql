f71ddb711a51984ee3b6ef0975e59917931988df
Lukey <ldipaola66@gmail.com>
2020-08-04 22:41:02 +1000

    added db schema and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers(burger) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger) VALUES ('Hamburger');
+INSERT INTO burgers (burger) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger) VALUES ('Beef and Bacon Burger');
+INSERT INTO burgers (burger) VALUES ('USA Burger');
+INSERT INTO burgers (burger) VALUES ('Aussie Burger');
