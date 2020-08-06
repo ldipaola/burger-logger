6b6b5dfdd3fd4b1b230edbb69fa4a8a3e3adf43c
Lukey <ldipaola66@gmail.com>
2020-08-06 22:56:04 +1000

    added require connection.js

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,5 @@
+const connection = require("../config/connection.js");
+
 // Helper function for SQL syntax.
 // Let's say we want to pass 3 values into the mySQL query.
 // In order to write the query, we need 3 question marks.
@@ -60,7 +62,7 @@ function printQuestionMarks(num) {
         const queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
 
     console.log(queryString);
-    connection.query(queryString, [table, col, col, condition], function(err, result) {
+    connection.query(queryString, [table, col, conditionCol, condition], function(err, result) {
       if (err) {
         throw err;
       }
