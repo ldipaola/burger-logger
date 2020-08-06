b871c71a00f6c56bc594fa24847faabe3d26965b
Lukey <ldipaola66@gmail.com>
2020-08-06 22:17:05 +1000

    added functions to interact with orm

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,25 @@
 // Import the ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js");
 
-// Export the database functions for the controller (burgerController.js).
 
+const burger = {
+    selectAll: (cb) => {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
+    insertOne: (col, val, cb) => {
+      orm.create("burgers", col, val, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: (objColVals, condition, cb) => {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+
+// Export the database functions for the controller (burgerController.js).
+module.exports = burger;
