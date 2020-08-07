08e2ce5ceb1f0541e4b7db051109276dc661547b
Lukey <ldipaola66@gmail.com>
2020-08-07 23:11:10 +1000

    added on click PUT request to button to  update burger in db

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,25 @@
 $(function() {
 
+  $(".devoured").on("click", function(event) {
+    const id = $(this).data("id");
+    const devoured = $(this).data("devoured");
+
+    const burgerEaten = {
+      eaten: true
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: burgerEaten
+    }).then(
+      function() {
+        console.log("Burger Eaten", burgerEaten);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
     $(".create-form").on("submit", function(event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
